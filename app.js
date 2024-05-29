6a0eb2ada5af437874ce46b4f105b3b5db1d9e52
Swethavenkatesan20 <157340837+Swethavenkatesan20@users.noreply.github.com>
2024-05-29 17:47:39 +0530

    Update app.js
    updated link

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const app=express();
 
 //enable cors request
 app.use(cors({
-    origin: 'http://localhost:5173',  // allow all origin
+    origin: 'https://capstone-vechicle-care-frontend-1.vercel.app',  // allow all origin
     credentials: true
 }));
 
